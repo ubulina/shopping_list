f18d8116ef625d62ff85136d2c0d821bc404583e
ubulina <ukokko@gmail.com>
2021-10-31 18:29:43 +0200

    Kauppalista-sovellus

--- App.js
+++ App.js
@@ -1,11 +1,43 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, FlatList } from 'react-native';
 
 export default function App() {
+
+  const [product, setProduct] = useState('')
+  const [data, setData] = useState([])
+
+  const addProduct = () => {
+
+    setData([...data, { key: product }])
+
+    setProduct('')
+  }
+
+  const clearList = () => {
+
+    setData([])
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput 
+          style={styles.input}
+          onChangeText={product => setProduct(product)}
+          value={product}          
+      />
+      <View style={styles.buttons}>
+        <Button onPress={addProduct} title="add" />
+        <Button onPress={clearList} title="clear" />
+      </View>
+      <Text style={styles.text}>Shopping list</Text>
+      <FlatList
+        data={data}
+        renderItem={({item}) => 
+          <Text>{item.key}</Text>}
+      />
+
       <StatusBar style="auto" />
     </View>
   );
@@ -17,5 +49,21 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginTop: 30
   },
+  input: {
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1
+      
+  },
+  text: {
+    color: '#0000FF',
+    fontWeight: 'bold'
+  },
+  buttons: {
+    flexDirection: 'row',
+    marginTop: 20,
+    marginBottom: 20
+  }
 });
